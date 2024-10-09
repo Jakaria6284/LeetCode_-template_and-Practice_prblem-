46c2107ebfa1576eff4364ba854f71dc52d1621d
Jakaria <101088391+Jakaria6284@users.noreply.github.com>
2024-10-09 16:31:49 +0600

    Time: 0 ms (100%), Space: 42.6 MB (19.54%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,6 +10,7 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
+        /*
 
         if(head==null || head.next==null)return head;
 
@@ -18,6 +19,21 @@ class Solution {
         head.next=null;
 
         return newHead;
+        */
+
+        ListNode prev=null;
+
+        ListNode current =head;
+
+        while(current!=null)
+        {
+            ListNode next=current.next;
+            current.next=prev;
+            prev=current;
+            current =next;
+        }
+
+        return prev;
         
     }
 }
