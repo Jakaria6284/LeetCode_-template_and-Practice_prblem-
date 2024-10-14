f858ed1a66861eb32102f72a458263b3e81cf8d9
Jakaria <101088391+Jakaria6284@users.noreply.github.com>
2024-10-14 12:25:37 +0600

    Time: 9 ms (21.21%), Space: 44.4 MB (52.92%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -38,3 +38,59 @@ class Solution {
         return dumy.next;
     }
 }
+
+/*
+class Solution {
+    
+    public ListNode mergeKLists(ListNode[] lists) {     
+        if(lists == null || lists.length == 0) {
+            return null;
+        }
+        
+        if(lists.length == 1) {
+            return lists[0];
+        }
+        
+        ListNode head = mergeTwoLinkedList(lists[0], lists[1]);
+        for(int i=2; i< lists.length; i++) {
+            head = mergeTwoLinkedList(head, lists[i]);
+        }
+        return head;
+    }
+
+    
+    //Merge two linkedList
+    private ListNode mergeTwoLinkedList(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+        
+        while(list1 != null && list2!= null) {
+            
+            if(list1.val < list2.val) {
+                dummy.next = list1;
+                list1 = list1.next;
+            } else {
+                dummy.next = list2;
+                list2 =list2.next;
+            }
+            
+            dummy = dummy.next;
+            
+        }
+        
+        while(list1 != null) {
+            dummy.next = list1;
+            list1 = list1.next;
+            dummy = dummy.next;
+        }
+        
+         while(list2 != null) {
+            dummy.next = list2;
+            list2 = list2.next;
+            dummy = dummy.next;
+        }
+        
+        return head.next;
+    }
+}
+*/
