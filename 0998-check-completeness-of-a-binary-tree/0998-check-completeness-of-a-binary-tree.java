434939a04f501a5578a109bd185f8f081f521547
Jakaria <101088391+Jakaria6284@users.noreply.github.com>
2024-11-29 12:00:35 +0600

    Time: 1 ms (86.37%), Space: 42.2 MB (35.37%) - LeetHub

--- 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.java
+++ 0998-check-completeness-of-a-binary-tree/0998-check-completeness-of-a-binary-tree.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean isCompleteTree(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList();
-        queue.add(root);
-        
-        while (!queue.isEmpty()) {
-            TreeNode node = queue.poll();
-			
-			// Trick here is that if you encounter  null between two non-null values, it ain't complete binary tree.
-            if (node == null && queue.peek() != null) return false; 
-            
-            if (node != null)  {
-                queue.add(node.left);
-                queue.add(node.right);
+       Queue<TreeNode>queue=new LinkedList<>();
+       queue.offer(root);
+
+       while(!queue.isEmpty())
+       {
+        int size=queue.size();
+        for(int i=0;i<size;i++)
+        {
+            TreeNode temp=queue.poll();
+            if(temp==null && queue.peek()!=null)return false;
+            if(temp!=null)
+            {
+                queue.offer(temp.left);
+                queue.offer(temp.right);
             }
         }
-        
-        return true;
+       }
+
+       return true;
     }
 }
