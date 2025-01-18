3ada5e4c31432c6c72462fb260ad52764b920595
Jakaria <101088391+Jakaria6284@users.noreply.github.com>
2025-01-19 00:16:55 +0600

    Time: 9 ms (17.52%), Space: 44.9 MB (30.96%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+
+        Map<String,Integer>map=new TreeMap<>();
+           PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+            (a, b) -> {
+                int valueComparison = b.getValue() - a.getValue(); // Compare by value (descending)
+                if (valueComparison != 0) {
+                    return valueComparison;
+                }
+                return a.getKey().compareTo(b.getKey()); // Compare by key (lexicographical order)
+            }
+        );
+        for(int i=0;i<words.length;i++)
+        {
+            map.put(words[i],map.getOrDefault(words[i],0)+1);
+        }
+
+         pq.addAll(map.entrySet());
+
+        List<String>list=new ArrayList<>();
+        for(int i=0;i<k;i++)
+        {
+            Map.Entry<String,Integer>val=pq.poll();
+            list.add(val.getKey());
+        }
+
+        return list;
+
+        
+    }
+}
