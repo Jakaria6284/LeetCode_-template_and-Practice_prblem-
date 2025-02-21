1729eae49be06323b1b17bc87138f2afda606387
Jakaria <101088391+Jakaria6284@users.noreply.github.com>
2025-02-21 23:22:28 +0600

    Time: 5 ms (84.03%), Space: 68.1 MB (35.92%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.java
@@ -1,34 +1,31 @@
+import java.util.*;
+
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> lessPivot = new ArrayList<>();
-        List<Integer> eqPivot = new ArrayList<>();
-        List<Integer> greaterPivot = new ArrayList<>();
-        List<Integer> res = new ArrayList<>();
+        int[] arr = new int[nums.length];
+        int index = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < pivot) {
-                lessPivot.add(nums[i]);
-            } else if (nums[i] > pivot) {
-                greaterPivot.add(nums[i]);
-            } else {
-                eqPivot.add(nums[i]);
+        // First pass: Add elements smaller than pivot
+        for (int num : nums) {
+            if (num < pivot) {
+                arr[index++] = num;
             }
         }
 
-        res.addAll(lessPivot);
-        res.addAll(eqPivot);
-        res.addAll(greaterPivot);
-
-        int []result=new int[res.size()];
-
-
-        for(int i=0;i<result.length;i++)
-        {
-            result[i]=res.get(i);
-
+        // Second pass: Add elements equal to pivot
+        for (int num : nums) {
+            if (num == pivot) {
+                arr[index++] = num;
+            }
         }
 
-        return result;
+        // Third pass: Add elements greater than pivot
+        for (int num : nums) {
+            if (num > pivot) {
+                arr[index++] = num;
+            }
+        }
 
+        return arr;
     }
 }
