7e74ca14f2a3b0969ad6b508cf555981236faf60
Jakaria <101088391+Jakaria6284@users.noreply.github.com>
2025-03-11 12:03:18 +0600

    Time: 1 ms (100%), Space: 44.3 MB (97.14%) - LeetHub

---
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        int m = baskets.length;
+        int count = 0;
+       // Arrays.sort(baskets);
+
+        for (int i = 0; i < n; i++) {
+            boolean place=false;
+            for (int j = 0; j < m; j++) {
+                if (fruits[i] <= baskets[j]) { 
+                    baskets[j]=-1;
+                    place=true;
+                    break;
+                    
+                }
+            }
+            if (!place) { // If no suitable basket found, count as unplaced
+                count++;
+            }
+        }
+        return count;
+    }
+}
