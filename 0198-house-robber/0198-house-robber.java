e015ecedb7c4e312f6972c24cd8f60c348aa86b6
Jakaria <101088391+Jakaria6284@users.noreply.github.com>
2025-03-22 22:22:39 +0600

    Time: 0 ms (100%), Space: 41.4 MB (15.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,37 +1,21 @@
 class Solution {
     public int rob(int[] nums) {
-        int index=nums.length;
-
         int dp[]=new int[nums.length+1];
-        Arrays.fill(dp,-1);
-       return recur(nums,index,dp);
+        dp[0]=0;
+        dp[1]=nums[0];
+
+        for(int i=2;i<dp.length;i++)
+        {
+            int pick=nums[i-1]+dp[i-2];
+            int notpick=0+dp[i-1];
+            dp[i]=Math.max(pick,notpick);
+        }
+
+        return dp[nums.length];
+
 
         
     }
 
-    public int recur(int []nums,int index,int dp[])
-    {
-        if(index==1)
-        {
-            dp[index]=nums[0];
-            return nums[0];
-        }
-        if(index==0)
-        {
-            dp[index]=0;
-            return 0;
-        }
-
-        if(dp[index]!=-1)
-        {
-            return dp[index];
-        }
-
-        int pick=nums[index-1]+recur(nums,index-2,dp);
-        int notPick=0+recur(nums,index-1,dp);
-
-        dp[index]= Math.max(pick,notPick);
-        return dp[index];
-
-    }
+   
 }
