7b071ea19f56484895a62cf9284a936226c8e97b
Jakaria <101088391+Jakaria6284@users.noreply.github.com>
2025-05-07 10:53:56 +0600

    Time: 1 ms (47.98%), Space: 57.1 MB (85.51%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,23 +1,25 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        k = k % n; // Handle cases where k > n
-        if (k == 0) return; // No rotation needed if k is 0
+        k = k % n;
+        if (k == 0)
+            return;
+        int lasthalf = n - k;
 
-        int[] arr = new int[n];
-        int j = 0;
+        int[] ans = new int[nums.length];
+        int idx = 0;
 
-        // Copy last k elements to the beginning
-        for (int i = n - k; i < n; i++) {
-            arr[j++] = nums[i];
+        for (int i = lasthalf; i < n; i++) {
+            ans[idx++] = nums[i];
         }
 
-        // Copy first n-k elements after the shifted elements
-        for (int i = 0; i < n - k; i++) {
-            arr[j++] = nums[i];
+        for (int i = 0; i < lasthalf; i++) {
+            ans[idx++] = nums[i];
         }
 
-        // Copy back to nums
-        System.arraycopy(arr, 0, nums, 0, n);
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = ans[i];
+        }
+
     }
 }
