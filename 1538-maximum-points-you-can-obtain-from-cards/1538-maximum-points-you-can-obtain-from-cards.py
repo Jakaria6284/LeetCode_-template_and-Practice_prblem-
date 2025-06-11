6430a77039848be75b034fc04b884a0080d98d6e
Jakaria <101088391+Jakaria6284@users.noreply.github.com>
2025-06-11 11:03:21 +0600

    Time: 32 ms (26.73%), Space: 28.1 MB (41.81%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.py
@@ -1,18 +1,16 @@
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        total = sum(cardPoints)  # total points of all cards
-        maxsum = 0
-        summ = 0
+        total=sum(cardPoints)
+        maxsum=0
+        summ=0
 
-        # Initial window of length n - k
-        for i in range(0, len(cardPoints) - k):
-            summ += cardPoints[i]
-        maxsum = total - summ  # max sum after removing that window
+        for i in range(0,len(cardPoints)-k):
+            summ+=cardPoints[i]
+        maxsum=total-summ
 
-        # Slide the window forward
-        for i in range(len(cardPoints) - k, len(cardPoints)):
-            summ += cardPoints[i]             # add next element to window
-            summ -= cardPoints[i - (len(cardPoints) - k)]  # remove first element of previous window
-            maxsum = max(maxsum, total - summ)  # update max
-
+        for i in range((len(cardPoints) - k),len(cardPoints)):
+            summ+=cardPoints[i]
+            summ-=cardPoints[i-(len(cardPoints) - k)]
+            maxsum=max(maxsum,total-summ)
         return maxsum
+        
