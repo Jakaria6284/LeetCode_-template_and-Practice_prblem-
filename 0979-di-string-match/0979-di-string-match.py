fba9fb77678047c185802f9cab5075998396c903
Jakaria <101088391+Jakaria6284@users.noreply.github.com>
2025-06-28 12:10:41 +0600

    Time: 3 ms (62.63%), Space: 18.7 MB (81.65%) - LeetHub

--- 0979-di-string-match/0979-di-string-match.py
+++ 0979-di-string-match/0979-di-string-match.py
@@ -1,20 +1,18 @@
 class Solution:
     def diStringMatch(self, s: str) -> List[int]:
-        perm_of_I=0
-        perm_of_D=len(s)
+        ei=0
+        d=len(s)
         res=[]
-        i=0
 
-        for i in range(len(s)):
+        for i in range(0,len(s)):
             if s[i]=='I':
-                res.append(perm_of_I)
-                perm_of_I+=1
-            else:
-                res.append(perm_of_D)
-                perm_of_D-=1
+                res.append(ei)
+                ei+=1
                 
-        res.append(perm_of_I)
-            
+            else:
+                res.append(d)
+                d-=1
+        res.append(d)
         return res
 
         
